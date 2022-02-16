2b48de0c65e27a14e9a63fd73eea0eb556565b3a
Ryan Reddy <90551370+klugook@users.noreply.github.com>
2022-02-16 23:47:38 +0100

    prog running - ready for testing

--- src/Practicum_3A/Main.java
+++ src/Practicum_3A/Main.java
@@ -1,4 +1,25 @@
 package Practicum_3A;
 
 public class Main {
+  public static void main(String[] args) {
+    VoetbalClub ajx = new VoetbalClub("Ajax      ", 0, 0, 0);
+    VoetbalClub feij = new VoetbalClub("Feijenoord", 0, 0, 0);
+
+    feij.verwerkResultaat('w');
+    feij.verwerkResultaat('w');
+    feij.verwerkResultaat('w');
+    feij.verwerkResultaat('g');
+
+//    ajx.verwerkResultaat('w');
+//    ajx.verwerkResultaat('w');
+//    ajx.verwerkResultaat('w');
+//    ajx.verwerkResultaat('w');
+
+    System.out.println("Feijenoord punten: " + feij.aantalPunten());
+    System.out.println("Ajax gespeeld: " + ajx.aantalGespeeld());
+    System.out.println();
+
+    System.out.println(ajx);
+    System.out.println(feij);
+  }
 }
--- src/Practicum_3A/VoetbalClub.java
+++ src/Practicum_3A/VoetbalClub.java
@@ -1,4 +1,56 @@
 package Practicum_3A;
 
+
 public class VoetbalClub {
+    private String naam;
+    private Integer gewonnen;
+    private Integer verloren;
+    private Integer gelijk;
+
+//    public Voetbalclub(String nm) {
+//        naam = nm;
+//    }
+
+    public VoetbalClub(String nm, Integer gw, Integer ver, Integer gel) {
+        naam = nm;
+        gewonnen = gw;
+        verloren = ver;
+        gelijk = gel;
+    }
+
+    public void setNaam(String nm) {naam = nm; }
+    public void setGewonnen(Integer gew) {
+        gewonnen = gew;}
+    public void setVerloren(Integer ver) {
+        verloren = ver;}
+    public void setGelijk(Integer gel) {
+        gelijk = gel;}
+
+    public String getNaam() { return naam; }
+    public Integer getGewonnen() { return gewonnen;}
+    public Integer getVerloren() { return verloren;}
+    public Integer getGelijk() { return gelijk;}
+
+    public Integer aantalPunten() {
+                return (gewonnen *3)+(gelijk);    }
+    public Integer aantalGelijk() {
+                return gelijk;    }
+    public Integer aantalGespeeld() {
+            return gelijk+gewonnen+verloren;    }
+
+
+    public void verwerkResultaat(char ch) {
+        if (ch == 'w')
+            gewonnen = gewonnen + 1;
+        if (ch == 'g')
+            gelijk = gelijk + 1;
+        if (ch == 'v')
+            verloren = verloren + 1;
+    }
+    public String toString() {
+        String s = naam + " " + aantalGespeeld() + " " + gewonnen + " " + gelijk + " " + verloren + " " + aantalPunten();
+        return s;
+    }
+
+
 }
