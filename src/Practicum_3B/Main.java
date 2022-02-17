618f4783f74f6d595d4d0f13d533f9a92a41214b
Ryan Reddy <90551370+klugook@users.noreply.github.com>
2022-02-17 22:08:45 +0100

    3B Cirkel werkend :D

--- src/Practicum_3B/Main.java
+++ src/Practicum_3B/Main.java
@@ -4,16 +4,16 @@ public class Main {
     public static void main(String[] args) {
         Cirkel c1 = null, c2 = null;
 
-        c1 = new Cirkel(10, 0, 0);
-        c2 = new Cirkel(0, 10, 10);
+//        testing throwing an exception without try-catcher
+//        c1 = new Cirkel(10, 0, 0);
+//        c2 = new Cirkel(0, 10, 10);
 
-
-//        try {
-//            c1 = new Cirkel(10, 0, 0);
-//            c2 = new Cirkel(0, 10, 10);
-//        } catch (IllegalArgumentException iae) {
-//            System.out.println(iae.getMessage());
-//        }
+        try {
+            c1 = new Cirkel(10, 0, 0);
+            c2 = new Cirkel(0, 10, 10);
+        } catch (IllegalArgumentException iae) {
+            System.out.println(iae.getMessage());
+        }
 
         System.out.println(c1);
         System.out.println(c2);
